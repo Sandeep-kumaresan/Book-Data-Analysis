07a68ee209656e0c93dddad63a7224c25afea8b9
Sandeep Kumaresan <sandeepkumaresan4676@gmail.com>
2024-09-16 18:00:47 +0530

    linechart3

--- deploy.py
+++ deploy.py
@@ -7,6 +7,12 @@ st.title("Welcome to Book Data Analysis!")
 df = pd.read_csv("Comics,Manga.csv")
 st.text("Comics and Manga Prices")
 st.bar_chart(data=df, x = "Price", y = "Title", x_label="Price", y_label="Book")
-st.line_chart(df["Price"])
+plt.figure(figsize=(12, 8))
+sns.countplot(data=df, x='Price', palette='viridis')
+plt.xticks(rotation=90)
+plt.title('Frequency of Each Book Price')
+plt.xlabel('Price')
+plt.ylabel('Count')
+st.pyplot(plt)
 
 
